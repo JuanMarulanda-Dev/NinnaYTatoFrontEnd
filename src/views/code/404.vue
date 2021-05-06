736de821d63e919b4cb2984cda524faceee0604e
= <juan.marulanda9811@gmail.com>
2021-05-06 18:40:17 -0500

    Add Page and svg 404

--- src/views/code/404.vue
+++ src/views/code/404.vue
@@ -1,3 +1,15 @@
 <template>
-  <div></div>
+  <div class="error">
+    <v-img src="/404.svg"></v-img>
+  </div>
 </template>
+
+<style scoped>
+.error {
+  background: #adadc9;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
