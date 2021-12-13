45f89c794e4e312bef9319eabecfe4c3e88a80f7
= <juan.marulanda9811@gmail.com>
2021-12-12 21:36:20 -0500

    cambiar svg por png

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -3,7 +3,7 @@ self.addEventListener("push", function (event) {
     var data = event.data.json();
     self.registration.showNotification(data.title, {
       body: data.body,
-      icon: "https://ninnaytato.s3.us-east-2.amazonaws.com/favicon.svg",
+      icon: "https://ninnaytato.s3.us-east-2.amazonaws.com/alerta.png",
     });
     console.log("This push event has data: ", event.data.text());
   } else {
