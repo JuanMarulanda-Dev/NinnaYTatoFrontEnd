f66b82ea1b718c149715728a78d23dde8e01cc4f
= <juan.marulanda9811@gmail.com>
2021-12-27 11:51:12 -0500

    new service worker

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,12 +1,18 @@
-self.addEventListener("push", function (event) {
-  if (event.data) {
-    var data = event.data.json();
-    self.registration.showNotification(data.title, {
-      body: data.body,
-      icon: "https://ninnaytato.s3.us-east-2.amazonaws.com/alerta.png",
-    });
-    console.log("This push event has data: ", event.data.text());
-  } else {
-    console.log("This push event has no data.");
-  }
-});
+import Vue from "vue";
+export const eventBus = new Vue();
+
+export const ServiceWorker = () => {
+  self.addEventListener("push", function (event) {
+    if (event.data) {
+      var data = event.data.json();
+      self.registration.showNotification(data.title, {
+        body: data.body,
+        icon: "favicon.png",
+      });
+      console.log("This push event has data: ", event.data.text());
+      eventBus.$emit("notification", data);
+    } else {
+      console.log("This push event has no data.");
+    }
+  });
+};
