687b16824d905658ab17a08468ba47b90e2c45ff
= <juan.marulanda9811@gmail.com>
2022-03-24 11:08:29 -0500

    change endpoint API to spa

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
   devServer: {
-    // host: "spa.d8ea-2800-e2-d80-1036-b049-cc3b-f7d-b0d2.ngrok.io",
+    // host: "192.168.1.57",
   },
 };
